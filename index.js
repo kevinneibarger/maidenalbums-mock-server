687591d72be7b25535601842bc52bcc6e63feb3f
kevinneibarger <kevinneibarger@hotmail.com>
2025-02-12 14:11:05 -0500

    Added authorization header

--- index.js
+++ index.js
@@ -3,11 +3,18 @@ import jsonServer from "json-server";
 import auth from "json-server-auth";
 
 const server = express();
+
 server.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*')
-    res.header('Access-Control-Allow-Headers', '*')
-    next()
-})
+    // Get the token from the request (e.g., from a query parameter or header)
+    const token = req.query.token || req.headers['authorization'];
+
+    if (token) {
+        // Add the authorization header to the request
+        req.headers['authorization'] = `Bearer ${token}`;
+    }
+
+    next(); // Pass control to the next middleware or route handler
+});
 
 const router = jsonServer.router('./data/dbAlbums.json');
 server.use('/api', router);
