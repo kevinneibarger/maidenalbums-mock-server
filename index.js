7d8481ff4468df9e730b1534bdf6709fd22622fc
kevinneibarger <kevinneibarger@hotmail.com>
2025-04-24 14:22:07 -0400

    Checked in changes for mock Node.js server to try to fix the login page.

--- index.js
+++ index.js
@@ -3,21 +3,17 @@ import jsonServer from "json-server";
 import auth from "json-server-auth";
 
 const server = express();
-
 server.use((req, res, next) => {
-    // Get the token from the request (e.g., from a query parameter or header)
-    const token = req.query.token || req.headers['authorization'];
-
-    res.header('Access-Control-Allow-Origin', '*')
-    res.header('Access-Control-Allow-Headers', '*')
-
-    if (token) {
-        // Add the authorization header to the request
-        req.headers['authorization'] = `Bearer ${token}`;
-    }
-
-    next(); // Pass control to the next middleware or route handler
-});
+    // res.header('Access-Control-Allow-Origin', '*')
+    // res.header('Access-Control-Allow-Headers', '*')
+    // next()
+    console.log(">>> GETTING HERE!!!! <<<<");
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-CSRF-Token, X-Requested-With, Origin, client-security-token, X-Requested-With, Content-Type, Accept, Authorization");
+    res.setHeader('Content-Type', 'application/json');
+    res.setHeader('Access-Control-Allow-Credentials', "true");
+    next();
+})
 
 const router = jsonServer.router('./data/dbAlbums.json');
 server.use('/api', router);
